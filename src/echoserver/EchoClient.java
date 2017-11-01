e94a715c52caa372bff9facf10d7eda0ea7fa6b7
scvstackkk <scv.stack@gmail.com>
2017-11-01 14:23:52 -0500

    Started working on client/server java files. Still need to get the input/output sorted out.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,47 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
 public class EchoClient {
+  
+  public static void main(String[] args) {
+    String hostname;
+
+    if (args.length == 0) {
+      System.out.println("Error: Forgot to supply hostname argument.");
+      return;
+    }
+    
+    else {
+       try {
+         hostname = args[0];
+	 Socket socket = new Socket(hostname, 50000);
+	 
+	 InputStream in = socket.getInputStream();
+	 OutputStream out = socket.getOutputStream();
+	 byte[] buffer = new byte[4096];
+	 
+	 int count;
+	 while ((count = in.read(buffer)) > 0) {
+	   out.write(buffer, 0, count);
+	 } 
+
+	 in.close();
+	 out.close();
+
+	 try {
+	    socket.close();
+	  } catch (IOException e) {
+	    System.out.println("Error closing socket @" + socket.getInetAddress().toString() + ".");
+	    System.out.println(e);
+	  }
+
+        } catch (IOException e) {
+	  System.out.println("Error.");
+	  System.out.println(e);
+	}
+    }
+  } 
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,51 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
+/*
+ * EchoServer uses a tcp connection to communicate with a client 
+ * on the specified port. The server reads client input, and
+ * writes it back to the client.
+ */
+
 public class EchoServer {
+
+  // Run the server
+  public static void main(String[] args) {
+    try {
+      ServerSocket listener = new ServerSocket(50000);
+      System.out.println("EchoServer is listening on port 50000.");
+
+      // Connect to a client
+      while (true) { 
+	 System.out.println("Waiting for client.");
+
+         Socket client = listener.accept();
+	 System.out.println("######--Connection established with client @" + client.getInetAddress().toString() + "--######");
+	 
+	 InputStream in = client.getInputStream();
+	 OutputStream out = client.getOutputStream();
+         byte[] buffer = new byte[4096];
+
+	 int count;
+	 while ((count = in.read(buffer)) > 0) {
+	   out.write(buffer, 0, count);
+	 }
+
+	 in.close();
+	 out.close();
+
+	 try {
+	   client.close();
+	 } catch (IOException e) {
+	   System.out.println("Error closing client socket @" + client.getInetAddress().toString() + ".");
+	   System.out.println(e);
+	 }
+      }	
+    } catch (IOException e) {
+      System.out.println("Error.");
+      System.out.println(e);
+    }
+  }
 }
